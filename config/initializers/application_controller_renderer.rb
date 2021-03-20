7db45df97a0d6a4dff8e8d6b1d567029eb69cbfa
haraguchi12 <masakazu09201201@gmail.com>
2021-03-20 08:58:54 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ec6dc96e586328d6370b2a8f1542c61e284faa64a4810570292e0038a98888791da92a16562205833de2ee9bc9046720fef77f6e2349ae89e00d6c6fb67a76b5'
